ac415d6a800558dd780f2da13f8c89a17eccd541
hide <nkret805worm@gmail.com>
2020-08-31 04:45:06 +0900

    delete components

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,9 @@
 <template>
   <div id="app">
-    <header-component/>
       <router-view/>
-    <footer-component/>
   </div>
 </template>
 
 <script>
-import HeaderComponent from './components/Header.vue'
-import FooterComponent from './components/Footer.vue'
 
-export default {
-  components: {
-    HeaderComponent,
-    FooterComponent
-  }
-}
 </script>
