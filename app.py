95fd0e39118f2893219b3f0aa9f594c6d19e2636
shin04 <daikon0413@gmail.com>
2020-08-31 06:03:52 +0900

    アキネーターAPI

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 
+from jackinator import Thothnator
+
 app = Flask(__name__)
 CORS(app)
 app.config['JSON_AS_ASCII'] = False
+
+thothnator = Thothnator()
 
 @app.route('/', methods=['GET'])
 def get_json_from_dictionary():
@@ -14,14 +18,29 @@ def get_json_from_dictionary():
 
 @app.route('/questions', methods=['GET'])
 def get_quesion():
-    queries = request.args
-    question = {"question": {"id": 1, "q": "aaa?", "ans": "yes", "end": False, "prediction": None}}
-    return jsonify(question)
+    q = thothnator.decideQ()
+    thothnator.q_list.append(q)
+    res = {"question": q}
+
+    return jsonify(res)
 
 @app.route('/questions', methods=['POST'])
 def post_question():
-    json = request.get_json()
-    return jsonify(json)
+    # count = request.json('count')
+    question = request.json['question']
+    answer = request.json['answer']
+    thothnator.a_list.append(answer)
+    thothnator.p = thothnator.updateP(thothnator.p, question, answer)
+
+    if thothnator.isNeedContinueQ():
+        result = ''
+        res = {'continue': True, 'result': result}
+    else:
+        result = thothnator.getBestEstimate()
+        thothnator.updateDatabase
+        res = {'continue': False, 'result': result}
+    
+    return jsonify(res)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
