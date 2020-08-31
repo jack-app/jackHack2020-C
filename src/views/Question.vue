de3755e1b23aa4c51912518b5d6aaba574d6eb08
hide <nkret805worm@gmail.com>
2020-08-31 14:27:37 +0900

    表示されないバグ解消

--- src/views/Question.vue
+++ src/views/Question.vue
@@ -3,7 +3,7 @@
     <v-img :src="jackinatorImage" max-height="250" max-width="180" contain class="jackImage"></v-img>
     <v-card flat class="question" max-height="380px" min-height="250px">
       <v-card-title>質問その{{ this.$route.params.id }}</v-card-title>
-      <v-card-text>{{ this.question.qestion }}</v-card-text>
+      <v-card-text>{{ questionSentence }}</v-card-text>
     </v-card>
   <v-row>
     <v-col cols="6">
@@ -19,24 +19,41 @@
 <script>
 import Question from '../models/question.js';
 import JackinatorImage from '../assets/jackinator.png';
+import axios from 'axios';
 
 export default{
   data() {
     return {
       question: new Question(),
+      questionS: new Question().question,
       jackinatorImage: JackinatorImage,
+    }
+  },
+  // mounted(){
+  //   console.log(this.question);
+  //   console.log(this.questionSentence);
+  // },
+  computed: {
+    questionSentence(){
+      return this.question.question;
     }
   },
   methods: {
     checkAnswer(answer) {
       console.log(this.question)
-      this.question.checkAnswer(answer).then( resp => {
+      axios.post( 'http://localhost:5000/questions', this.toPostedObject(answer) ).then( resp => {
         if ( resp.data.continue ) {
           this.$router.go({path: this.$router.currentRoute.path, force: true});
         } else {
           this.$router.push({ path: `/result`, params: {result: resp.data.result} })
         }
       } )
+    },
+    toPostedObject(answer){
+      var postedObject = {}
+      postedObject.question = this.questionS;
+      postedObject.answer = answer;
+      return postedObject;
     }
   }
 
