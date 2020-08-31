a524da7eff6594d30641d857cfeed0fcadf1d8f3
hide <nkret805worm@gmail.com>
2020-08-31 11:18:48 +0900

    homeのデザイン調整

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,29 @@
 <template>
-  <div>
-    <div>
-      <v-img></v-img>
-    </div>
-    <div class="">
-      <h1>Jackinator</h1>
+  <v-container class="d-flex justify-center">
+    <div class="title">
+      <v-img :src="jackinatorTitleImage"></v-img>
     </div>
     <div>
+      <v-img :src="jackinatorImage" max-width="180px" max-height="270px" contain class="jackImage"></v-img>
+    </div>
+    <div class="caution">
       <h3>素直に答えてね</h3>
     </div>
-    <div class="d-flex justify-center">
-      <v-btn @click="toQuestion">私にチャレンジ<br/><span>あなたの生まれ年を当ててみせます</span></v-btn>
+    <div class="Btn">
+      <v-btn @click="toQuestion" class="mainBtn" width="270px" height="75px" color="#FFA95A">私にチャレンジ<br>あなたの生まれ年を当ててみせます</v-btn>
     </div>
-  </div>
+  </v-container>
 </template>
 <script>
+import JackinatorTitleImage from '../assets/JackinatorTitle.png';
+import JackinatorImage from '../assets/jackinator.png';
 export default{
+  data(){
+    return{
+      jackinatorTitleImage: JackinatorTitleImage,
+      jackinatorImage: JackinatorImage
+    }
+  },
   methods: {
     toQuestion(){
       this.$router.push({ path: `/question/${1}` })
@@ -24,4 +32,30 @@ export default{
 }
 </script>
 <style scoped>
+.title{
+  margin: 0 auto;
+  margin-top: 40px !important;
+  margin-bottom: 30px !important;
+}
+.caution{
+  height: 65px;
+  margin: 30px auto;
+  text-align: center;
+  width: 90%;
+  background: linear-gradient(90deg, rgba(146, 146, 146, 0.49) 0%, rgba(196, 196, 196, 0.63) 50.55%, rgba(146, 146, 146, 0) 102.17%);
+}
+.jackImage{
+  margin: 0 auto;
+}
+h3{
+  color: white;
+  text-align: center;
+  line-height: 65px;
+}
+.mainBtn{
+  white-space: pre;
+}
+.Btn{
+  text-align: center;
+}
 </style>
